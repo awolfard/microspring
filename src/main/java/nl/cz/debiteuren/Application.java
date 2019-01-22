5bb984c78d91e015383ab8b172299da53ee038d6
Arnoud <a.wolfard@home.nl>
2019-01-23 00:12:08 +0100

    Application.properties added

--- src/main/java/nl/cz/debiteuren/Application.java
+++ src/main/java/nl/cz/debiteuren/Application.java
@@ -2,6 +2,7 @@ package nl.cz.debiteuren;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	
+	@Value("${application.name}")
+	private String applicationName;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -19,6 +23,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
+            System.out.println("Starting up " + applicationName);
             System.out.println("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
